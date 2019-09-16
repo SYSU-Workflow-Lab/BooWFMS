489c0014d0f56b0d8555e6692f50bd2077325c48
SkyeBeFreeman <928016560@qq.com>
2019-09-16 17:50:16 +0800

    initialize project

---
+++ boo-engine/src/main/java/cn/edu/sysu/workflow/BooEngineApplication.java
@@ -0,0 +1,16 @@
+package cn.edu.sysu.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Skye on 2019/9/16.
+ */
+@SpringBootApplication
+public class BooEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooEngineApplication.class, args);
+    }
+
+}

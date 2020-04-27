3a4d79c20e1e482722a03ae12a6940af056659c1
skyebefreeman <928016560@qq.com>
2020-04-27 17:41:44 +0800

    初始化access模块的zuul功能

--- boo-access/src/main/java/cn/edu/sysu/workflow/access/BooAccessApplication.java
+++ boo-access/src/main/java/cn/edu/sysu/workflow/access/BooAccessApplication.java
@@ -2,11 +2,13 @@ package cn.edu.sysu.workflow.access;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Skye
  * Created on 2019/9/16
  */
+@EnableZuulProxy
 @SpringBootApplication
 public class BooAccessApplication {
 
